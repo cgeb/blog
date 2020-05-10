c801c9360cf8e1b0ffaee98893c20dbc824e4084
cgeb <ckg61386@gmail.com>
2020-05-10 11:04:47 +0900

    convert article content to use ActionText

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
 
   validates :title, :content, presence: true
+  has_rich_text :content
 
   def self.search(term)
     return all unless term
 
-    Article.where("lower(title) LIKE :term OR lower(content) LIKE :term", term: "%#{term.downcase}%")
+    Article.where("lower(title) LIKE :term", term: "%#{term.downcase}%")
   end
 end
